4892ddc57d150dc5ea750cae6563390172e5b828
vivek-annu <springboot.ai@gmail.com>
2019-07-29 08:45:48 +0530

    other files

---
+++ datastructure-core-java/src/main/java/com/vivek/ds/OnGeekEventListener.java
@@ -0,0 +1,52 @@
+package com.vivek.ds;
+// Java program to illustrate synchronous callback 
+interface OnGeekEventListener { 
+
+	// this can be any type of method 
+	void onGeekEvent(); 
+} 
+
+class B { 
+
+	private OnGeekEventListener mListener; // listener field 
+
+	// setting the listener 
+	public void registerOnGeekEventListener(OnGeekEventListener mListener) 
+	{ 
+		this.mListener = mListener; 
+	} 
+
+	// my synchronous task 
+	public void doGeekStuff() 
+	{ 
+
+		// perform any operation 
+		System.out.println("Performing callback before synchronous Task"); 
+
+		// check if listener is registered. 
+		if (this.mListener != null) { 
+
+			// invoke the callback method of class A 
+			mListener.onGeekEvent(); 
+		} 
+	} 
+
+	// Driver Function 
+	public static void main(String[] args) 
+	{ 
+		B obj = new B(); 
+		OnGeekEventListener mListener = new A(); 
+		obj.registerOnGeekEventListener(mListener); 
+		obj.doGeekStuff(); 
+	} 
+} 
+
+class A implements OnGeekEventListener { 
+
+	public void onGeekEvent() 
+	{ 
+		System.out.println("Performing callback after synchronous Task"); 
+		// perform some routine operation 
+	} 
+	// some class A methods 
+} 
